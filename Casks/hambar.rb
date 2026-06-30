df3fc6e0934dc85094c7a5ee3bfc0f0d44b03a2b
Hipszki Janos <janos.hipszki@udemx.eu>
2026-06-30 09:20:47 +0200

    Update hambar cask to 1.5

--- Casks/hambar.rb
+++ Casks/hambar.rb
@@ -1,6 +1,6 @@
 cask "hambar" do
-  version "1.4"
-  sha256 "7a3d1957334617c0b2b2bd428621cf7fdbbc0ea153c12e0c2f4a7078058ba9a6"
+  version "1.5"
+  sha256 "bb226ce680e81cf08e3d826d71f40916c134f69bc447e00f9db7ea1e28629934"
 
   url "https://github.com/hipszkij/hambar-app/releases/download/v#{version}/HAmbar-#{version}.zip"
   name "HAmbar"
